e75da1bd90e7e88355d556a528aebf72a70c76bc
hatake04 <michaelangelo002@hotmail.com>
2021-04-13 08:45:55 -0400

    Update date.js

--- date.js
+++ date.js
@@ -1,2 +1,12 @@
-var currentDate = new Date()
+var SPRING_REG = "Nov 25 Jan 21";
+var FALL_REG = "Jun 25 Aug 21";
+let currentDate = new Date();
 let dateArray = (currentDate.toString()).split();
+
+let spring = SPRING_REG.split();
+let fall = FALL_REG.split();
+let springS = new Date(spring[0], spring[1], dateArray[3]);
+let springF = new Date(spring[2], spring[3], dateArray[3]);
+let fallS = new Date(fall[0], fall[1], dateArray[3]);
+if(currentDate < springS.getTime() && currentDate > springF.getTime())
+  console.log("Invalid Operation");
