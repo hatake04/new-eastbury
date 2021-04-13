96fc2f6f5977c05fe01bb0acf40be8587e58a5ab
hatake04 <michaelangelo002@hotmail.com>
2021-04-13 13:54:13 -0400

    Update date.js

--- date.js
+++ date.js
@@ -1,12 +1,42 @@
-var SPRING_REG = "Nov 25 Jan 21";
-var FALL_REG = "Jun 25 Aug 21";
-let currentDate = new Date();
-let dateArray = (currentDate.toString()).split(" ");
 
-let spring = SPRING_REG.split();
-let fall = FALL_REG.split();
-let springS = new Date(spring[0], spring[1], dateArray[3]);
-let springF = new Date(spring[2], spring[3], dateArray[3]);
-let fallS = new Date(fall[0], fall[1], dateArray[3]);
-if(currentDate < springS.getTime() && currentDate > springF.getTime())
-  console.log("Invalid Operation");
+function registrationPeriod(semester)
+{
+  let springDates = "10 23 0 21"; //start date month and end date and month 
+  let fallDates = "05 23 07 21";
+  let date = new Date();	//current date
+  let currentDate = (date.toString()).split(" ");
+
+  let spring = springDates.split(" ");
+  let fall = fallDates.split(" ");
+
+  let springS = new Date(currentDate[3], spring[0], spring[1]); //Date object registration Spring starts
+  let springF = new Date(currentDate[3], spring[2], spring[3]); // Date object registration Spring ends
+  let fallS = new Date(currentDate[3], fall[0], fall[1]);	//Date object registration Fall starts
+  let fallF = new Date(currentDate[3], fall[2], fall[3]); //Date object registration Fall ends.
+
+	switch(semester)
+  {
+  			case 0: {
+    	 			if(date.getTime() < springS.getTime() || date.getTime > springF.getTime()) // compares registration time window for spring with current date
+  					{
+    						alert("Registration not in session.");
+    						window.location.href = "";
+  					}
+  					else
+   							window.open("");
+      			break;
+
+    		}
+ 				case 1: 
+  			{
+   					if(date.getTime() < fallS.getTime() || date.getTime > fallF.getTime()) // compares registration time window for spring with current date
+  					{
+    						alert("Registration not in session.");
+    						window.location.href = "";
+  					}
+  					else
+    						window.open("");
+    				break;
+       	}
+ 	}
+}
