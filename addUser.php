148c65b7b5794f1978740eea4fc6321eec5fbf50
hatake04 <michaelangelo002@hotmail.com>
2021-04-13 07:44:29 -0400

    Update addUser.php

--- addUser.php
+++ addUser.php
@@ -1,5 +1,14 @@
+
+<?php
 $check = "SELECT U_ID FROM USER WHERE USER_ID = ?;";
 $result = mysqli_query($conn, $check);
 if(mysqli_num_rows($result) > 0)
-  echo("User already in the database.");
+  echo("User exits in the database.");
+else
+{
+  //block of code
+}
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
  
