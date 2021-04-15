0c113011190802af9a4a13c362c329bed2eb76e1
hatake04 <michaelangelo002@hotmail.com>
2021-04-15 13:09:32 -0400

    Create viewClass.php

---
+++ viewClass.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>View class</h2>
+<form method="POST">
+	<label>CRN</label>
+    <input type="text">
+</form>
+<?php
+	include "connectToDB.php";
+    extract($_POST);
+    
+    $query = "SELECT {placeholder} FROM ClASSES;";
+    $result = mysqli_query($conn, $query);
+    if(mysqli_num_rows($result) > 0)
+    {
+    	$row = mysqli_fetch_assoc($result);
+    	echo("<p>{placeholder}</p>");
+    }
+    mysqli_free_result($result);
+    mysqli_close($conn);
+?>
+
+</body>
+</html>
