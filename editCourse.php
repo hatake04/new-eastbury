5990b455df35c2ae556e52d91270e58e62ba4ed7
hatake04 <michaelangelo002@hotmail.com>
2021-04-16 14:48:54 -0400

    Update editCourse.php

--- editCourse.php
+++ editCourse.php
@@ -5,10 +5,11 @@
 	include "connectToDB.php";
     switch($_POST['submit'])
     {
-    	extract($_POST);
+    
     	case "crs_Name":
         {
-          $query = "UPDATE COURSES SET Crs_Name = '$name' WHERE '$_SESSION["Course_ID"]';";
+
+          $query = "UPDATE COURSES SET Crs_Name = ". '$name' . " WHERE  " . '$_SESSION["Course_ID"]' . ";";
           $result = mysqli_query($conn, $query);
           if(mysqli_affected_rows($result) > 0)
               echo("<p>Course name changed to " .$name. " </p>");
@@ -20,7 +21,7 @@
     
     	case "crs_Desc":
         {
-        	$query = "UPDATE COURSES SET Crs_Desc = '$message' WHERE '$_SESSION["Course_ID"]';";
+        	$query = "UPDATE COURSES SET Crs_Desc = ". '$message' ."  WHERE " . '$_SESSION["Course_ID"]' . ";";
           	$result = mysqli_query($conn, $query);
           	if(mysqli_affected_rows($result) > 0)
               echo("<p>Course description changed to " .$name. " </p>");
@@ -31,7 +32,7 @@
         }
     	case "crs_Credit":
         {
-        	$query = "UPDATE COURSES SET Crs_Credit = '$credit' WHERE $_SESSION["Course_ID"];";
+        	$query = "UPDATE COURSES SET Crs_Credit = " . '$credit' . " WHERE " . $_SESSION["Course_ID"] . ";";
           	$result = mysqli_query($conn, $query);
           	if(mysqli_affected_rows($result) > 0)
               echo("<p>Course name credits changed to " .$credit. " </p>");
@@ -45,7 +46,7 @@
         	$subQuery = "SELECT Dept_ID FROM DEPARMENT WHERE Dept_Name = '$departments';";
             $subResult = mysqli_query($conn, $subQuery);
             $subRow = mysqli_fetch_assoc(subResult);
-        	$query = "UPDATE COURSES SET Dept_ID = 'subRow["Dept_ID"]' WHERE $_SESSION["Course_ID"];";
+        	$query = "UPDATE COURSES SET Dept_ID = " . 'subRow["Dept_ID"]' ." WHERE ".$_SESSION["Course_ID"] .";";
           	$result = mysqli_query($conn, $query);
           	if(mysqli_affected_rows($result) > 0)
               echo("<p>{placeholder}</p>");
