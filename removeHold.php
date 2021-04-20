e81b8eba02163ae8c729ff0474512a80d84cd93d
hatake04 <michaelangelo002@hotmail.com>
2021-04-20 11:31:52 -0400

    Create removeHold.php

---
+++ removeHold.php
@@ -0,0 +1,10 @@
+<?php
+        //implement on delete cascade
+    $query = "DELETE FROM HOLD WHERE Hold_ID = {holdPlaceholder};";
+    $result = mysqli_query($conn, $query);
+    if(mysqli_affected_rows($conn) == 0)
+    {
+        echo("Error in deleting record. Try again.");
+    }
+   
+?>
